c5a8f33d99557c4055ce8c25110ff065a7c81689
Leandro <leandro.bargas025@gmail.com>
2022-08-17 20:24:57 -0300

    Add user choice to the game

--- javascript.js
+++ javascript.js
@@ -1,10 +1,43 @@
 function getComputerChoice (){
-    let random = Math.floor(Math.random()*10);
-    if (random < 3)
+    let random = Math.floor(Math.random()*3);
+    if (random == 1)
     return "Rock"
-    else if (random <6)
+    else if (random == 2)
     return "Paper"
     else
     return "scissors"
   }
 
+  function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === "rock") {
+      if (computerSelection.toLowerCase() === "paper")
+      return "You lose! Paper beats rock"
+      else if (computerSelection.toLowerCase() === "scissors")
+      return "You win! Rock beats scissors"
+      else
+      return "It's a tie!"
+    } 
+    else if (playerSelection.toLowerCase() === "scissors") {
+      if (computerSelection.toLowerCase() === "paper")
+      return "You win! Scissors beats paper"
+      else if (computerSelection.toLowerCase() === "rock")
+      return "You lose! Rock beats scissors"
+      else
+      return "It's a tie!"
+    } 
+    else if (playerSelection.toLowerCase() === "paper") {
+      if (computerSelection.toLowerCase() === "rock")
+      return "You Win! Paper beats rock"
+      else if (computerSelection.toLowerCase() === "scissors")
+      return "You lose! Scissors beats paper"
+      else
+      return "It's a tie!"
+    } 
+    else
+    return prompt("Choose Rock, Paper or Scissors")
+    
+  }
+   
+  const playerSelection = prompt("Choose Rock, Paper or Scissors");
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
