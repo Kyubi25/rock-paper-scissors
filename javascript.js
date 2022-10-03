2abbbd093599ade7a440ff5ab8f42a60f89084ac
Leandro <leandro.bargas025@gmail.com>
2022-10-03 12:28:19 -0300

    Resolve game logic

--- javascript.js
+++ javascript.js
@@ -1,46 +1,72 @@
-const playerSelection = prompt("Choose Rock, Paper or Scissors");
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+let rounds = 5;
+const outcomes = ['rock' , 'paper' , 'scissors'];
 
-function getComputerChoice (){
-    let random = Math.floor(Math.random()*3);
-    if (random == 1)
-    return "Rock"
-    else if (random == 2)
-    return "Paper"
-    else
-    return "Scissors"
+function computerSelect(){
+  let runningStuff = computerPlay();
+  computerSelection = runningStuff;
+  return computerSelection;
+}
+
+function game(){
+  let i; 
+    for (i = 0; i < rounds; i++ ){
+      computerSelect();
+      playerSelect();
+      validName();
+    }
+}
+
+function playerSelect(){
+  let initialPrompt = prompt('Please enter your choise:');
+  playerSelection = initialPrompt.toLowerCase();
+  return playerSelection;
+}
+
+function validName(){
+  if (outcomes.includes(playerSelection)){
+    playRound(playerSelection, computerSelection);
+  } else {
+    alert("Invalid Selection, please try again!");
+    rounds = rounds +1;
+    return rounds;
   }
+}
+
+function computerPlay(){
+  return outcomes[Math.floor(Math.random() * outcomes.length)];
+}
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() === "rock") {
-      if (computerSelection.toLowerCase() === "paper")
-      return "You lose! Paper beats rock"
-      else if (computerSelection.toLowerCase() === "scissors")
-      return "You win! Rock beats scissors"
-      else
-      return "It's a tie!"
-    } 
-    else if (playerSelection.toLowerCase() === "scissors") {
-      if (computerSelection.toLowerCase() === "paper")
-      return "You win! Scissors beats paper"
-      else if (computerSelection.toLowerCase() === "rock")
-      return "You lose! Rock beats scissors"
-      else
-      return "It's a tie!"
-    } 
-    else if (playerSelection.toLowerCase() === "paper") {
-      if (computerSelection.toLowerCase() === "rock")
-      return "You Win! Paper beats rock"
-      else if (computerSelection.toLowerCase() === "scissors")
-      return "You lose! Scissors beats paper"
-      else
-      return "It's a tie!"
-    } 
-    else
-    return "Try again!"
+  switch( true || false) {
+    case playerSelection === "rock"  && computerSelection === "paper":
+      alert ("Computer chose paper so you lose! ");
+      break;
     
+    case playerSelection === "paper" && computerSelection === "scissors":
+      alert ("Computer chose scissors so you lose! ");
+      break;
+
+      case  playerSelection === "scissors" && computerSelection === "rock":
+        alert("Computer chose rock and you lose!");
+    break;
+
+    case playerSelection === "rock" && computerSelection === "scissors":
+        alert("Computer chose scissors and you win!");
+    break;
+
+    case playerSelection === "paper" && computerSelection === "rock":
+        alert("Computer chose rock and you win!");
+    break;
+
+    case playerSelection === "scissors" && computerSelection === "paper":
+        alert("Computer chose paper and you win!");
+    break;
+    
+    case playerSelection === computerSelection:
+        alert("It's a tie!");
+    break;
   }
+}
 
-
+game();
   
